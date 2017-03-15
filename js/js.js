525fab9d3d1cb47b408ef4fe23b7cd0670299279
Marija Robinson <marija@springmail.com>
2017-03-15 18:37:37 -0500

    Init: Initial commit

---
+++ js/js.js
@@ -0,0 +1,6 @@
+function fadePicsIn() {
+    document.getElementById('picture').style.opacity='1';
+    document.getElementById('projects-container').style.opacity='1';
+}
+
+window.onload = fadePicsIn;
