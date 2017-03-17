e85a95c4f0daa00d11b3afb7a0fb7437c02dd2ec
Marija Robinson <marija@springmail.com>
2017-03-17 13:53:48 -0500

    remove: Remove js.js file
    Not needed

--- js/js.js
+++
@@ -1,6 +0,0 @@
-function fadePicsIn() {
-    document.getElementById('picture').style.opacity='1';
-    document.getElementById('projects-container').style.opacity='1';
-}
-
-window.onload = fadePicsIn;
